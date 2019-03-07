39b5fcf5fac72eda6070319c885be9d369ccacb0
VenkateshSR <venkateshsr20@gmail.com>
2019-03-06 21:02:01 -0500

    Added UI for booking

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -9,6 +9,8 @@ import { FlightService } from '../flight.service';
 export class BookingComponent implements OnInit {
 
   booking :any;
+  users :any;
+  flights :any;
   constructor(private flightService : FlightService) { }
 
   ngOnInit() {
@@ -19,10 +21,22 @@ export class BookingComponent implements OnInit {
       financialInstrument : null,
       totalAmount : 0
     }
+    this.getUsers();
+    this.getFlights();
   }
 
   onSubmit(){
     this.flightService.saveBooking(this.booking,true)
     .subscribe(data =>console.log(), error => console.log(error));
+  }
+
+  getUsers(){
+    this.flightService.getUsers()
+    .subscribe(data =>this.users=data, error => console.log(error));
+  }
+
+  getFlights(){
+    this.flightService.getFlights()
+    .subscribe(data =>this.flights=data, error => console.log(error));
   }
 }
